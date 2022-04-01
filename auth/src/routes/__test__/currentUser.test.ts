89e12f205231cfee7772c6ea465b4f1ba6119259
Rodrigo Otávio <rodrigo.vieira@serpro.gov.br>
2022-04-01 01:02:17 -0300

    fix test

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -8,7 +8,7 @@ it("returns the user object when signed in", async () => {
     .get("/api/users/currentUser")
     .set("Cookie", cookie)
     .send()
-    .expect(400);
+    .expect(200);
 
   expect(response.body.currentUser.email).toEqual("test@test.com");
 });
